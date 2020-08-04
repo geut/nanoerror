e3aa7070c41712c1b2c857fdc40d29d8233af233
Martin Acosta <tinchoz49@gmail.com>
2020-08-04 01:08:00 -0300

    Added JSDoc

--- index.js
+++ index.js
@@ -1,28 +1,77 @@
 const format = require('quick-format-unescaped')
 
-module.exports = function createError (code, message = '%s') {
-  return class extends Error {
+class Nanoerror extends Error {
+  /**
+   * @static
+   * @param {Nanoerror} err
+   * @returns {boolean}
+   */
+  static equals (err) {
+    return err && typeof err === 'object' && err.isNanoerror
+  }
+
+  /**
+   * Creates a new Error
+   * @param {string} code
+   * @param {string} unformatMessage
+   * @param {Array<any>} [args]
+   */
+  constructor (code, unformatMessage, args = []) {
+    super(format(unformatMessage, args))
+
+    /** @type {string} */
+    this.name = code
+    /** @type {code} */
+    this.code = code
+    /** @type {Array<any>} */
+    this.args = args
+    /** @type {string} */
+    this.unformatMessage = unformatMessage
+
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, this.constructor)
+    } else {
+      this.stack = (new Error(this.message)).stack
+    }
+  }
+
+  /**
+   * @readonly
+   * @returns {boolean}
+   */
+  get isNanoerror () {
+    return true
+  }
+}
+
+/**
+ * Creates a new Error class
+ *
+ * @param {string} code
+ * @param {string} message
+ */
+function createError (code, message = '%s') {
+  /**
+   * @mixin
+   */
+  return class extends Nanoerror {
+    /**
+     * @static
+     * @param {Nanoerror} err
+     * @returns {boolean}
+     */
     static equals (err) {
-      return err instanceof Error && err.isNanoerror && err.code === code
+      return super.equals(err) && err.code === code
     }
 
+    /**
+     * Creates a new Error
+     * @param {...any} [args]
+     */
     constructor (...args) {
-      super(format(message, args))
-
-      this.name = code
-      this.code = code
-      this.args = args
-      this.unformatMessage = message
-
-      if (typeof Error.captureStackTrace === 'function') {
-        Error.captureStackTrace(this, this.constructor)
-      } else {
-        this.stack = (new Error(this.message)).stack
-      }
-    }
-
-    get isNanoerror () {
-      return true
+      super(code, message, args)
     }
   }
 }
+
+module.exports = createError
